5a04dfebf9a5e261c5da62168bbb510c84d0b837
1801-AD-2TK36M2\Outreach24 <jerbail31@gmail.com>
2018-07-31 14:34:58 -0400

    update

--- notepad/app/scripts/notepad.js
+++ notepad/app/scripts/notepad.js
@@ -1,33 +1,38 @@
-var notenumber = 1
+var notenumber1 = 1;
+var notenumber2 = 1;
+var notenumber3 = 1;
+var noteinput1 = "";
+var noteinput2 = "";
+var noteinput3 = "";
 $("#createnote").click(function() {
 	$("#newnote").css('display', 'block');
 });
 $("#save").click(function() {
-	if (notenumber==1) {
+	if (notenumber1==1) {
 	var noteinput1 = $("#noteenter").val();
 	var titleinput1 = $("#titleenter").val();
 	$("#title1").append(titleinput1);
 	$("#note1").css('display', 'block');
 	$("#newnote").css('display', 'none');
-	notenumber++
+	notenumber1++
 	}
 	else{
-		if(notenumber==2){
+		if(notenumber2==1){
 		var noteinput2 = $("#noteenter").val();
 		var titleinput2 = $("#titleenter").val();
 		$("#title2").append(titleinput1);
 		$("#note2").css('display', 'block');
 		$("#newnote").css('display', 'none');
-		notenumber++
+		notenumber2++
 		}
 		else{
-			if(notenumber==3) {
+			if(notenumber3==1) {
 			var noteinput3 = $("#noteenter").val();
 			var titleinput3 = $("#titleenter").val();
 			$("#title3").append(titleinput1);
 			$("#note3").css('display', 'block');
 			$("#newnote").css('display', 'none');
-			notenumber++
+			notenumber3++
 			}
 			else {
 				alert("You have reached the maximum number of notes")
@@ -49,6 +54,25 @@ function view3() {
 function deletenote1() {
 	if(confirm("are you sure you want to delete the note?")){
 		$("#note1").css('display', 'none');
+		notenumber1 = 1;
+	}
+	else{
+		//do nothing
+	}
+}
+function deletenote2() {
+	if(confirm("are you sure you want to delete the note?")){
+		$("#note2").css('display', 'none');
+		notenumber2 = 1;
+	}
+	else{
+		//do nothing
+	}
+}
+function deletenote3() {
+	if(confirm("are you sure you want to delete the note?")){
+		$("#note3").css('display', 'none');
+		notenumber3 = 1;
 	}
 	else{
 		//do nothing
