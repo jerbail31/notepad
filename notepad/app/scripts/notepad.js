9b5dd03f4f1e8ad8a000668bd2f5a0ff50107656
1801-AD-2TK36M2\Outreach24 <jerbail31@gmail.com>
2018-07-31 13:11:20 -0400

    update

--- notepad/app/scripts/notepad.js
+++ notepad/app/scripts/notepad.js
@@ -1,4 +1,13 @@
+var noteinput;
+var titleinput;
 $("#createnote").click(function() {
 	$("#newnote").css('display', 'block');
 });
-
+$("#save").click(function() {
+	debugger;
+	noteinput = $("#noteenter").val();
+	titleinput = $("#titleenter").val();
+	$("#title").append(titleinput);
+	$("#note").css('display', 'block');
+	
+});
