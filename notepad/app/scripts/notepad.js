92f9e667f85e5276cf2035cbd40b34f6d0477c2c
1801-AD-2TK36M2\Outreach24 <jerbail31@gmail.com>
2018-07-31 13:53:12 -0400

    update

--- notepad/app/scripts/notepad.js
+++ notepad/app/scripts/notepad.js
@@ -1,13 +1,42 @@
-var noteinput;
-var titleinput;
+var note# = 1
 $("#createnote").click(function() {
 	$("#newnote").css('display', 'block');
 });
 $("#save").click(function() {
-	debugger;
-	noteinput = $("#noteenter").val();
-	titleinput = $("#titleenter").val();
-	$("#title").append(titleinput);
-	$("#note").css('display', 'block');
-	
+	if (note#==1) {
+	var noteinput1 = $("#noteenter").val();
+	var titleinput1 = $("#titleenter").val();
+	$("#title1").append(titleinput1);
+	$("#note1").css('display', 'block');
+	$("#newnote").css('display', 'none');
+	}
+	else{
+		if(note#==2){
+		var noteinput1 = $("#noteenter").val();
+		var titleinput1 = $("#titleenter").val();
+		$("#title1").append(titleinput1);
+		$("#note1").css('display', 'block');
+		$("#newnote").css('display', 'none');
+		}
+		else{
+			if(note#==3) {
+			var noteinput1 = $("#noteenter").val();
+			var titleinput1 = $("#titleenter").val();
+			$("#title1").append(titleinput1);
+			$("#note1").css('display', 'block');
+			$("#newnote").css('display', 'none');
+			}
+			else {
+				alert("You have reached the maximum number of notes")
+			}	
+		}
+	}
+
 });
+function view() {
+	alert(noteinput1);
+}
+function deletenote() {
+	confirm("are you sure you want to delete the note?")
+
+}
