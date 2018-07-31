1effc203f55de694b5b3394ed1b28e3add6aad15
1801-AD-2TK36M2\Outreach24 <jerbail31@gmail.com>
2018-07-31 14:04:14 -0400

    update

--- notepad/app/scripts/notepad.js
+++ notepad/app/scripts/notepad.js
@@ -9,22 +9,25 @@ $("#save").click(function() {
 	$("#title1").append(titleinput1);
 	$("#note1").css('display', 'block');
 	$("#newnote").css('display', 'none');
+	notenumber++
 	}
 	else{
-		if(notenunber==2){
-		var noteinput1 = $("#noteenter").val();
-		var titleinput1 = $("#titleenter").val();
-		$("#title1").append(titleinput1);
-		$("#note1").css('display', 'block');
+		if(notenumber==2){
+		var noteinput2 = $("#noteenter").val();
+		var titleinput2 = $("#titleenter").val();
+		$("#title2").append(titleinput1);
+		$("#note2").css('display', 'block');
 		$("#newnote").css('display', 'none');
+		notenumber++
 		}
 		else{
 			if(notenumber==3) {
-			var noteinput1 = $("#noteenter").val();
-			var titleinput1 = $("#titleenter").val();
-			$("#title1").append(titleinput1);
-			$("#note1").css('display', 'block');
+			var noteinput3 = $("#noteenter").val();
+			var titleinput3 = $("#titleenter").val();
+			$("#title3").append(titleinput1);
+			$("#note3").css('display', 'block');
 			$("#newnote").css('display', 'none');
+			notenumber++
 			}
 			else {
 				alert("You have reached the maximum number of notes")
